6b4442001ea3c5a241c84a50afba8fcac9fc7de5
SantoshThakare <santoshthakare50@gmail.com>
2022-02-16 22:54:03 +0530

    SingleCastDelegates

--- DelegateProgram/Program.cs
+++ DelegateProgram/Program.cs
@@ -8,35 +8,12 @@ namespace DelegateProgram
 {
     internal class Program
     {
+        public delegate void operation(int x, int y);
         static void Main(string[] args)
         {
-            bool flag = true;
-            while (flag)
-            {
-                Console.WriteLine( "\n 1.Multi Delegates."
-                                + "\n 2.Exit\n");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
-                {
-                    
-                    case 1:
-                        Console.WriteLine("Multi Cast Delegates");
-                        DelOp delOp = MultiCastDelegates.Add;
-                        delOp += MultiCastDelegates.Sub;
-                        delOp += MultiCastDelegates.Mul;
-                        delOp += MultiCastDelegates.Div;
-                        delOp(15, 5);
-                        delOp(9, 3);
-                        break;
-                    case 2:
-                        flag = false;
-                        break;
-                    default:
-                        Console.WriteLine("Please Enter correct option");
-                        break;
-                }
-            }
-            Console.ReadLine();
+            Console.WriteLine("Single Cast Delegates");
+            operation obj = MultiCastDelegates.Add;
+            obj(15, 5);
         }
     }
 }
---
+++ DelegateProgram/SingleCastDelegates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProgram
+{
+    public delegate void DelOp(int x, int y);
+     class MultiCastDelegates
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine("Addition of 2 numbers       : {0}", a + b);
+            Console.ReadLine();
+        }
+    }
+    
+    
+}
+        
+

7809123a5a9f312e45421175c262499e3e949253
SantoshThakare <santoshthakare50@gmail.com>
2022-02-16 22:37:30 +0530

    MulticastDelegates

---
+++ DelegateProgram/MultiDelegates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProgram
+{
+    public delegate void DelOp(int x, int y);
+    internal class MultiCastDelegates
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine("Addition of 2 numbers       : {0}", a + b);
+        }
+        public static void Sub(int a, int b)
+        {
+            Console.WriteLine("Substraction of 2 numbers   : {0}", a - b);
+        }
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine("Multiplication of 2 numbers : {0}", a * b);
+        }
+        public static void Div(int a, int b)
+        {
+            Console.WriteLine("Division of 2 numbers       : {0}", a / b);
+        }
+    }
+}
+        
+
---
+++ DelegateProgram/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine( "\n 1.Multi Delegates."
+                                + "\n 2.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    
+                    case 1:
+                        Console.WriteLine("Multi Cast Delegates");
+                        DelOp delOp = MultiCastDelegates.Add;
+                        delOp += MultiCastDelegates.Sub;
+                        delOp += MultiCastDelegates.Mul;
+                        delOp += MultiCastDelegates.Div;
+                        delOp(15, 5);
+                        delOp(9, 3);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter correct option");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
